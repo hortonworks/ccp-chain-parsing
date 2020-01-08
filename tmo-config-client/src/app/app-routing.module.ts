f7a11b20557ff6dd6141bf53a9b5b500aad7dd35
Tibor Meller <tibor.meller@gmail.com>
2020-01-08 17:38:35 +0100

    BUG-122711 Adding routing

--- tmo-config-client/src/app/app-routing.module.ts
+++ tmo-config-client/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { ChainListPageComponent } from './chain-list-page/chain-list-page.component';
 import { MainContainerComponent } from './misc/main/main.container';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   { path: '404', component: PageNotFoundComponent },
   { path: '', component: MainContainerComponent },
+  { path: 'parserconfig', component: ChainListPageComponent },
   { path: ':type', component: MainContainerComponent },
   { path: '**', component: PageNotFoundComponent },
 ];
