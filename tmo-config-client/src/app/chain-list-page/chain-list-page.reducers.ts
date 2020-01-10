930cbb8a3ca2874f86d371dd25dc57a10af0b5cd
Subhash Jha <subhashjha35@gmail.com>
2020-01-10 23:52:28 +0100

    fixed one typo (in reducers)

--- tmo-config-client/src/app/chain-list-page/chain-list-page.reducers.ts
+++ tmo-config-client/src/app/chain-list-page/chain-list-page.reducers.ts
@@ -53,7 +53,7 @@ export function reducer(
         items: [...state.items, action.chain]
       };
     }
-    case chainListPageActions.DELETE_CHAIN_FAIL: {
+    case chainListPageActions.CREATE_CHAIN_FAIL: {
       return {
         ...state,
         error: action.error.message,
