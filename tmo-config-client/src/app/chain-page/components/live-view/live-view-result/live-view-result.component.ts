a6e723e0fdbe84eafab218022cfc0cd3ddbb0967
Shane Ardell <shane.m.ardell@gmail.com>
2020-01-30 18:37:15 -0500

    BUG-122900: fix import order

--- tmo-config-client/src/app/chain-page/components/live-view/live-view-result/live-view-result.component.ts
+++ tmo-config-client/src/app/chain-page/components/live-view/live-view-result/live-view-result.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
 import { LiveViewResultModel } from '../models/live-view.model';
 
 @Component({
