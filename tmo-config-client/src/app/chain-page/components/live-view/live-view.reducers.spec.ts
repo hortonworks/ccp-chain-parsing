fcd1edcaa13c852754d296d57171650a8883bedc
Tibor Meller <tibor.meller@gmail.com>
2020-02-03 13:31:02 +0100

    BUG-122899 Extending live view reducer tests

--- tmo-config-client/src/app/chain-page/components/live-view/live-view.reducers.spec.ts
+++ tmo-config-client/src/app/chain-page/components/live-view/live-view.reducers.spec.ts
@@ -1,12 +1,26 @@
-import { sampleDataChanged } from './live-view.actions';
+import { sampleDataChanged, chainConfigChanged, executionTriggered, liveViewRefreshedSuccessfully, liveViewRefreshFailed } from './live-view.actions';
 import { LiveViewActionsType } from './live-view.actions';
 import { initialState, reducer } from './live-view.reducers';
 import { SampleDataModel } from './models/sample-data.model';
 
-describe('live-view.reducers', () => {
+fdescribe('live-view.reducers', () => {
+
+  const testConfigState = {
+    id: '123',
+    name: 'abcd',
+    parsers: []
+  };
+
+  const testLiveViewState = { 
+    sampleData: new SampleDataModel(), 
+    parserChainConfig: testConfigState,
+    result: {
+      entries: []
+    },
+  }
 
   it('should handle default case', () => {
-    expect(reducer(initialState, { type: undefined } as LiveViewActionsType)).toBe(initialState);
+    expect(reducer(undefined, { type: undefined } as LiveViewActionsType)).toBe(initialState);
   });
 
   it('should update sample data on sampleDataChanged action', () => {
@@ -15,6 +29,41 @@ describe('live-view.reducers', () => {
 
     const newState = reducer(initialState, sampleDataChanged({ sampleData }));
     expect(newState.liveViewModel.sampleData.source).toBe('test input');
+  });
+
+  it('should update isModelDirty on chainConfigChanged action', () => {
+    const newState = reducer(initialState, chainConfigChanged({ chainConfig: testConfigState }));
+    expect(newState.isModelDirty).toBe(true);
+  });
+
+  it('should update isExecuting on executionTriggered action', () => {
+    const newState = reducer(initialState, executionTriggered());
+    expect(newState.isExecuting).toBe(true);
+  });
+
+  it('should update isExecuting on liveViewRefreshedSuccessfully action', () => {
+    const newState = reducer(initialState, liveViewRefreshedSuccessfully({ liveViewResult: testLiveViewState }));
+    expect(newState.isExecuting).toBe(false);
+  });
+
+  it('should update isModelDirty on liveViewRefreshedSuccessfully action', () => {
+    const newState = reducer(initialState, liveViewRefreshedSuccessfully({ liveViewResult: testLiveViewState }));
+    expect(newState.isModelDirty).toBe(false);
+  });
+
+  it('should update liveViewModel on liveViewRefreshedSuccessfully action', () => {
+    const newState = reducer(initialState, liveViewRefreshedSuccessfully({ liveViewResult: testLiveViewState }));
+    expect(newState.liveViewModel).toBe(testLiveViewState);
+  });
+
+  it('should update isExecuting on liveViewRefreshFailed action', () => {
+    const newState = reducer(initialState, liveViewRefreshFailed({ error: { message: 'ups' } }));
+    expect(newState.isExecuting).toBe(false);
+  });
+
+  it('should update isModelDirty on liveViewRefreshFailed action', () => {
+    const newState = reducer(initialState, liveViewRefreshFailed({ error: { message: 'ups' } }));
+    expect(newState.isModelDirty).toBe(true);
   });
 
 });
