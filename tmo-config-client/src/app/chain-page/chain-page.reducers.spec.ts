960b1d03ab2f323e0112f9f11a28059b0fc9080f
tfodor430@gmail.com <tfodor430@gmail.com>
2020-02-05 13:35:30 +0100

    add reducer tests

--- tmo-config-client/src/app/chain-page/chain-page.reducers.spec.ts
+++ tmo-config-client/src/app/chain-page/chain-page.reducers.spec.ts
@@ -70,4 +70,117 @@ describe('chain-page: reducers', () => {
       error: ''
     });
   });
+
+  it('load success: should set all the components', () => {
+    const state = {
+      chains: null,
+      parsers: null,
+      routes: null,
+      error: ''
+    };
+    const chains = {};
+    const parsers = {};
+    const routes = {};
+    const newState = fromReducers.reducer(state, new fromActions.LoadChainDetailsSuccessAction({
+      chains,
+      parsers,
+      routes
+    }));
+    expect(newState.chains).toBe(chains);
+    expect(newState.parsers).toBe(parsers);
+    expect(newState.routes).toBe(routes);
+  });
+
+  it('should update the given parser', () => {
+    const state = {
+      chains: null,
+      parsers: {
+        456: {
+          id: '456',
+          type: 'grok',
+          name: 'some parser',
+          outputs: 'old',
+          config: 'old'
+        }
+      },
+      routes: null,
+      error: ''
+    };
+    const newState = fromReducers.reducer(state, new fromActions.UpdateParserAction({
+      parser: {
+        id: '456',
+        outputs: 'new',
+        config: 'new'
+      }
+    }));
+    expect(newState.parsers['456']).toEqual({
+      id: '456',
+      type: 'grok',
+      name: 'some parser',
+      outputs: 'new',
+      config: 'new'
+    });
+  });
+
+  it('should return with the desired parser', () => {
+    const desiredParser = {
+      id: '456',
+      type: 'grok',
+      name: 'some parser',
+      outputs: 'old',
+      config: 'old'
+    };
+    const state = {
+      'chain-page': {
+        chains: null,
+        parsers: {
+          456: desiredParser
+        },
+        routes: null,
+        error: ''
+      }
+    };
+    const parser = fromReducers.getParser(state, { id: '456' });
+    expect(parser).toBe(desiredParser);
+  });
+
+  it('should return with the desired chain', () => {
+    const desiredChain = {
+      id: '456',
+      name: 'some chain',
+      parsers: []
+    };
+    const state = {
+      'chain-page': {
+        chains: {
+          456: desiredChain
+        },
+        parsers: null,
+        routes: null,
+        error: ''
+      }
+    };
+    const chain = fromReducers.getChain(state, { id: '456' });
+    expect(chain).toBe(desiredChain);
+  });
+
+  it('should return with the desired route', () => {
+    const desiredRoute = {
+      id: '456',
+      name: 'some route',
+      subchain: ''
+    };
+    const state = {
+      'chain-page': {
+        chains: null,
+        parsers: null,
+        routes: {
+          456: desiredRoute
+        },
+        error: ''
+      }
+    };
+    const route = fromReducers.getRoute(state, { id: '456' });
+    expect(route).toBe(desiredRoute);
+  });
 });
