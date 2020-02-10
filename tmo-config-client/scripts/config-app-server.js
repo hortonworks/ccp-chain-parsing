5e667b1b181266b631d344805db6f71e4c7e176f
Shane Ardell <shane.m.ardell@gmail.com>
2020-02-10 09:47:14 -0500

    BUG-123032: use host values instead of env vars in production

--- tmo-config-client/scripts/config-app-server.js
+++ tmo-config-client/scripts/config-app-server.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 let path = require("path");
 const HTML = path.join(__dirname, "../dist/tmo-parser-chaining/");
