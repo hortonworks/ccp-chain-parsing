ddc6d654e6a7c8fe2a0086ebe37ef1f0b7a5cc98
Nick Allen <nickallen@apache.org>
2020-02-13 10:04:55 -0500

    Fixed constant in FieldValueTest

--- parser-chains-core/src/test/java/com/cloudera/parserchains/core/FieldValueTest.java
+++ parser-chains-core/src/test/java/com/cloudera/parserchains/core/FieldValueTest.java
@@ -7,8 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class FieldValueTest {
-
-    static String value = "<14>1 2014-06-20T09:14:07+00:00 loggregator"
+    private static final String value = "<14>1 2014-06-20T09:14:07+00:00 loggregator"
             + " d0602076-b14a-4c55-852a-981e7afeed38 DEA MSG-01"
             + " [exampleSDID@32473 iut=\"3\" eventSource=\"Application\" eventID=\"1011\"]"
             + "[exampleSDID@32480 iut=\"4\" eventSource=\"Other Application\" eventID=\"2022\"] "
