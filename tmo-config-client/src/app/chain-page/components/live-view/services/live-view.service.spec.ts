5efc0db2c0b18e511cdff29a504b7d0866ac250b
Shane Ardell <shane.m.ardell@gmail.com>
2020-02-27 14:17:30 -0500

    BUG-123031: update live view service with API updates

--- tmo-config-client/src/app/chain-page/components/live-view/services/live-view.service.spec.ts
+++ tmo-config-client/src/app/chain-page/components/live-view/services/live-view.service.spec.ts
@@ -37,7 +37,7 @@ describe('LiveViewService', () => {
       service.SAMPLE_PARSER_URL,
       {
         sampleData: { type: SampleDataType.MANUAL, source: 'test sample input' },
-        parserChainSchema: { id: '456', name: 'gdf', parsers: [] }
+        chainConfig: { id: '456', name: 'gdf', parsers: [] }
       }
     );
   });
--- tmo-config-client/src/app/chain-page/components/live-view/services/live-view.service.ts
+++ tmo-config-client/src/app/chain-page/components/live-view/services/live-view.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-import { EntryParsingResultModel } from '../models/live-view.model';
+import { EntryParsingResultModel, LiveViewModel } from '../models/live-view.model';
 import { SampleDataModel } from '../models/sample-data.model';
 
 @Injectable({
@@ -10,15 +10,15 @@ import { SampleDataModel } from '../models/sample-data.model';
 })
 export class LiveViewService {
 
-  readonly SAMPLE_PARSER_URL = '/api/v1/parserconfig/parser-test';
+  readonly SAMPLE_PARSER_URL = '/api/v1/parserconfig/tests';
 
   constructor(
     private http: HttpClient,
   ) { }
 
-  execute(sampleData: SampleDataModel, parserChainSchema: {}): Observable<{ results: EntryParsingResultModel[]}> {
+  execute(sampleData: SampleDataModel, chainConfig: {}): Observable<{ results: EntryParsingResultModel[]}> {
     return this.http.post<{ results: EntryParsingResultModel[]}>(
       this.SAMPLE_PARSER_URL,
-      { sampleData, parserChainSchema } as any);
+      { sampleData, chainConfig } as LiveViewModel);
   }
 }
