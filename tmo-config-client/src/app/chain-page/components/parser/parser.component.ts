97c2006eb5ae8b09a1a93877d915bbbf57a162f6
Subhash Jha <subhashjha35@gmail.com>
2020-03-12 15:31:35 +0100

    added collapsing feature in parser container

--- tmo-config-client/src/app/chain-page/components/parser/parser.component.ts
+++ tmo-config-client/src/app/chain-page/components/parser/parser.component.ts
@@ -121,4 +121,8 @@ export class ParserComponent implements OnInit, OnChanges {
     this.parserChange.emit({ id: this.parser.id, config: event.value });
   }
 
+  stopEventPropagation(event: Event) {
+    event.stopPropagation();
+  }
+
 }
