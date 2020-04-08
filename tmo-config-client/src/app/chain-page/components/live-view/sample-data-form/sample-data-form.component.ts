d5790be786421fe9de1a1689a69156aafc3ac16a
Shane Ardell <shane.m.ardell@gmail.com>
2020-04-08 13:56:20 -0400

    add button to upload sample data from file

--- tmo-config-client/src/app/chain-page/components/live-view/sample-data-form/sample-data-form.component.ts
+++ tmo-config-client/src/app/chain-page/components/live-view/sample-data-form/sample-data-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { NzMessageService } from 'ng-zorro-antd';
 
 import { SampleDataModel } from '../models/sample-data.model';
 
@@ -11,12 +12,46 @@ export class SampleDataFormComponent {
 
   @Input() sampleData: SampleDataModel;
   @Output() sampleDataChange = new EventEmitter<SampleDataModel>();
+  @ViewChild('sampleDataInput', { static: true }) sampleDataInput: ElementRef;
+
+  constructor(private messageService: NzMessageService) {}
 
   onApply(sampleDataInput: string) {
     this.sampleDataChange.emit({
       ...this.sampleData,
       source: sampleDataInput
     });
+  }
+
+  uploadToForm(e) {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onload = () => {
+      const fileTypeError = this.checkFileType(file);
+      if (fileTypeError) {
+        this.messageService.create('error', fileTypeError.message);
+        return;
+      }
+      this.sampleDataInput.nativeElement.value = reader.result;
+      this.onApply(this.sampleDataInput.nativeElement.value);
+    };
+    reader.readAsText(file);
+  }
+
+  private checkFileType(file: File) {
+    if (!file) {
+      return null;
+    }
+    const [, extension] = file.name.split('.');
+    const fileExt = ['txt', 'csv'];
+    const fileTypes = ['text/plain', 'text/csv'];
+
+    if (!fileExt.find(ext => ext === extension.toLowerCase()) || !fileTypes.find(type => file.type === type)) {
+      return {
+        message: `The file must be a .txt or .csv`
+      };
+    }
+    return null;
   }
 
 }
